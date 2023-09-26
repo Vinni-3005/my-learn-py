7ff309733c7e1c27c7fa36459af8542a423a5a4c
Vinni-3005 <vinithaaduri@gmail.com>
2023-09-26 12:46:08 +0530

    ok

---
+++ Datatypes/mutable.py
@@ -0,0 +1,24 @@
+# 1.lists 
+
+num=[10,20,30,40,50,60]
+print(num)
+str=["banana","apple","orange"]
+print(str)
+
+# 2.sets
+ids={101,102,103}
+print(ids) 
+
+id={101,101,102,102,103,103,104,104}
+print(id)
+
+
+# 3.dict
+emp={'id':101,
+     'name':'vinni',
+     'sal':45000
+     }
+print(emp)
+
+
+
