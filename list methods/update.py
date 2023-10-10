dc74ebb85de52aaebb0e27ac3c2ad9885cd8c100
Vinni-3005 <vinithaaduri@gmail.com>
2023-10-10 13:30:08 +0530

    lists

---
+++ list methods/update.py
@@ -0,0 +1,21 @@
+#create 
+#ids=[101,102,103,104]
+#ids=list()
+#words="Hello,Good morning".split()
+#list=eval(input("enter num"))
+
+
+#read
+#1.using indexing
+#ids[0]
+#ids[1]
+#ids[2]
+#ids[3]
+
+#2.slice operator(:)
+#3.for, while
+
+#update
+ids=[101,102,103,104]
+ids.append(105)
+print(ids)
