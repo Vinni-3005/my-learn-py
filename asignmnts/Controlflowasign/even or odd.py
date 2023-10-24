9103e5b239b2bdbca3b2b1612d907a51688393c7
Vinni-3005 <vinithaaduri@gmail.com>
2023-10-24 14:56:45 +0530

    done

---
+++ asignmnts/Controlflowasign/even or odd.py
@@ -0,0 +1,14 @@
+#case 1 using if else
+'''num=int(input("enter a number"))
+if(num%2==0):
+    print("It is even number")
+else:
+    print("It is odd number")'''
+
+#case 2 using for
+number=int(input("Enter a number"))
+for num in range(number+1):
+    if(num%2==0):
+        print("It is even number")
+    else:
+        print("It is odd number")
---
+++ asignmnts/Controlflowasign/greatest.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter first number"))
+num2=int(input("Enter a second number"))
+
+if num1 > num2:
+    print("num1 greater than num2")
+elif num1==num2:
+    print("Both numbers are equal")
+else: 
+    print("num2 greater than num1")
